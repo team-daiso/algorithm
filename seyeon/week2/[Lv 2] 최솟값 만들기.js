4b4ae1fee4c50ab2aeed63f129b89be3fa59aeff
uxolrv <uxolrv@gmail.com>
2023-01-11 17:14:32 +0900

    [Lv 2] 최솟값 만들기 (리팩터링)

--- seyeon/week2/[Lv 2] 최솟값 만들기.js
+++ seyeon/week2/[Lv 2] 최솟값 만들기.js
@@ -1,13 +1,8 @@
 function solution(A, B) {
-  let result = 0;
   // 오름차순으로 A 정렬
-  const acsendingA = A.sort((a, b) => a - b);
+  A.sort((a, b) => a - b);
   // 내림차순으로 B 정렬
-  const descendingB = B.sort((a, b) => b - a);
+  B.sort((a, b) => b - a);
 
-  for (let i = 0; i < A.length; i++) {
-    result += acsendingA[i] * descendingB[i];
-  }
-
-  return result;
+  return A.reduce((acc, cur, i) => acc + cur * B[i], 0);
 }
