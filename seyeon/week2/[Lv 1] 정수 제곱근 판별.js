541a3ad235f0cf3b2dfc03a58cf02a23d4f18258
uxolrv <uxolrv@gmail.com>
2023-01-11 17:13:21 +0900

    [Lv 1] 정수 제곱근 판별 (리팩터링)

--- seyeon/week2/[Lv 1] 정수 제곱근 판별.js
+++ seyeon/week2/[Lv 1] 정수 제곱근 판별.js
@@ -1,5 +1,6 @@
 function solution(n) {
   const sqrt = Math.sqrt(n);
-
-  return sqrt % 1 > 0 ? -1 : (sqrt + 1) ** 2;
+  // 제곱근이 정수가 아니라면, 1로 나눴을 때 나머지가 존재
+  // 나머지가 있으면 -1, 아니면 sqrt+1의 제곱을 리턴
+  return sqrt % 1 ? -1 : (sqrt + 1) ** 2;
 }
