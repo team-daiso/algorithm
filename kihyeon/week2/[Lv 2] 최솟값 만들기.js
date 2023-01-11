d19f29fe17eccd89503b9743fca1dca0e422d6fd
kihyeoon <bangion93@gmail.com>
2023-01-11 18:13:48 +0900

    [Lv 2] 최솟값 만들기 - 리팩터링01

--- kihyeon/week2/[Lv 2] 최솟값 만들기.js
+++ kihyeon/week2/[Lv 2] 최솟값 만들기.js
@@ -1,7 +1,6 @@
 function solution(A, B) {
-  var answer = 0;
-  A = A.sort((a, b) => a - b);
-  B = B.sort((a, b) => b - a);
-  for (let i = 0; i < A.length; i++) answer += A[i] * B[i];
-  return answer;
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  return A.reduce((acc, cur, i) => acc + cur * B[i], 0);
 }
