d7194db425d59f678aa510a96b2f2d50d6f1e2cf
uxolrv <uxolrv@gmail.com>
2023-01-18 17:02:18 +0900

    [Lv 2] 이진 변환 반복하기 (리팩터링)

--- seyeon/week3/[Lv 2] 이진 변환 반복하기.js
+++ seyeon/week3/[Lv 2] 이진 변환 반복하기.js
@@ -1,14 +1,12 @@
 function solution(s) {
-  // 0을 제거한 배열
-  let oneCollection = [];
   // 이진 변환의 결과
   let binary = s;
   let count = [0, 0];
 
   while (binary !== '1') {
-    oneCollection = binary.split('').filter((num) => num === '1');
-    count[1] += binary.length - oneCollection.length;
-    binary = oneCollection.length.toString(2);
+    oneArrLength = binary.split('').filter((num) => num === '1').length;
+    count[1] += binary.length - oneArrLength;
+    binary = oneArrLength.toString(2);
     count[0]++;
   }
 
