1664618ae8c8ab60aea02f3d61412f4e5fd52a2c
uxolrv <uxolrv@gmail.com>
2023-01-25 22:49:54 +0900

    [Lv 1] 콜라츠 추측 (리팩터링)

--- seyeon/week4/[Lv 1] 콜라츠 추측.js
+++ seyeon/week4/[Lv 1] 콜라츠 추측.js
@@ -1,8 +1,6 @@
 let count = 0;
 
 function solution(num) {
-  if (num === 1) return 0;
-
   return collatz(num);
 }
 
@@ -10,8 +8,7 @@ function collatz(num) {
   if (num === 1) return count;
   if (500 <= count) return -1;
 
-  if (num % 2 === 0) num /= 2;
-  else num = num * 3 + 1;
+  num = num % 2 === 0 ? num / 2 : num * 3 + 1;
 
   count++;
 
