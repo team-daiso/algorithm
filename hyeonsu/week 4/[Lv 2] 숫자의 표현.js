a2905e49ed3bda1f0f2b4516433d262e76b84888
dohyeons <ddhhss0603@gmail.com>
2023-01-25 18:15:25 +0900

    들여쓰기

--- hyeonsu/week 4/[Lv 2] 숫자의 표현.js
+++ hyeonsu/week 4/[Lv 2] 숫자의 표현.js
@@ -1,26 +1,26 @@
 // 효율성에서 걸렸습니다.
 function solution(n) {
-	// 가볍게 2중 반복문으로 ㄱ
-	var answer = 0;
-	for(j = 1 ; j <= n ; j ++) {
-			let result = 0;
-			for(let i = j; result < n ; i ++) {
-							result += i
-			}
-			if(result === n) {
-					answer += 1
-			}
-	}
-	return answer;
+  // 가볍게 2중 반복문으로 ㄱ
+  var answer = 0;
+  for (j = 1; j <= n; j++) {
+    let result = 0;
+    for (let i = j; result < n; i++) {
+      result += i;
+    }
+    if (result === n) {
+      answer += 1;
+    }
+  }
+  return answer;
 }
 
 // 풀었어여. 연속된 숫자의 합이 n이려면 그 조합의 갯수 = n의 홀수인 약수의 갯수 래여
 function solution(n) {
-	let answer = 1;
-	for(let i  = 2 ; i <= n ; i ++) {
-			if(i % 2 === 1 && n % i === 0) {
-					answer++
-			}
-	}
-	return answer;
+  let answer = 1;
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 === 1 && n % i === 0) {
+      answer++;
+    }
+  }
+  return answer;
 }
--- hyeonsu/week 4/[Lv 2] 피보나치 수.js
+++ hyeonsu/week 4/[Lv 2] 피보나치 수.js
@@ -1,8 +1,8 @@
-let memo = [0, 1]
+let memo = [0, 1];
 
 function solution(n) {
-    for(let i = 2; i <= n ; i ++) {
-        memo.push( (memo[i-1] + memo[i-2]) % 1234567 )
-    }
-  return memo[n]
+  for (let i = 2; i <= n; i++) {
+    memo.push((memo[i - 1] + memo[i - 2]) % 1234567);
+  }
+  return memo[n];
 }
