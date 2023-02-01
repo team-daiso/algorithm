3f24ae78d14aa2d320556207e5a170c2c9fc87d9
dohyeons <ddhhss0603@gmail.com>
2023-02-01 14:28:09 +0900

    [Lv 1] 가운데 글자 가져오기 - 필요없는 Math.floor 삭제

--- hyeonsu/week 5/[Lv 1] 가운데 글자 가져오기.js
+++ hyeonsu/week 5/[Lv 1] 가운데 글자 가져오기.js
@@ -3,5 +3,5 @@ function solution(s) {
   // 짝수면 가운데 두글자
   return s.length % 2 !== 0
     ? s[Math.floor(s.length / 2)]
-    : s[Math.floor(s.length / 2 - 1)] + s[Math.floor(s.length / 2)];
+    : s[(s.length / 2 - 1)] + s[(s.length / 2)];
 }
