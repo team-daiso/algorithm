56e51a4cd56f2ed1ec288f6991b0c7b2122a8db6
kihyeoon <bangion93@gmail.com>
2023-02-01 23:02:55 +0900

    [Lv 2] 짝지어 제거하기 - 수정

--- kihyeon/week5/[Lv 2] 짝지어 제거하기.js
+++ kihyeon/week5/[Lv 2] 짝지어 제거하기.js
@@ -2,7 +2,7 @@ function solution(s) {
     let stack = [];
     for (let i = 0; i < s.length; i++) {
         let current = s[i];
-        if (stack.length > 0 && stack[stack.length - 1] === current) {
+        if (stack[stack.length - 1] === current) {
             stack.pop();
         } else {
             stack.push(current);
