e43c2bebeb43371c16d54dbb4279c734ff9e97b0
uxolrv <uxolrv@gmail.com>
2023-02-01 23:47:48 +0900

    [Lv 1] 제일 작은 수 제거하기 (풀이 추가)

--- week5/[Lv 1] 제일 작은 수 제거하기.js
+++ week5/[Lv 1] 제일 작은 수 제거하기.js
@@ -5,8 +5,17 @@ function solution(arr) {
   return arr.length ? arr : [-1];
 }
 
-// 이건 훨씬 오래 걸리네요!
+// 이건 훨씬 오래 걸리네요!  ==> Math.min(...arr)을 변수로 따로 빼주면 됨
 function solution(arr) {
   const result = arr.filter((num) => num !== Math.min(...arr));
   return result.length ? result : [-1];
+}
+
+// Math.min(...arr) 변수로 선언
+function solution(arr) {
+  if (arr.length === 1) return [-1];
+
+  const min = Math.min(...arr);
+
+  return arr.filter((num) => num !== min);
 }
