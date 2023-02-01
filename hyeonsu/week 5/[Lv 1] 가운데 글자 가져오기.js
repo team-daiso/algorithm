44e666e4aa7b76bb1595911815c49c3b7bc51fc1
dohyeons <ddhhss0603@gmail.com>
2023-02-01 11:01:16 +0900

    [Lv 1] 가운데 글자 가져오기

---
+++ hyeonsu/week 5/[Lv 1] 가운데 글자 가져오기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+  // 홀수면 가운데 글자
+  // 짝수면 가운데 두글자
+  return s.length % 2 !== 0
+    ? s[Math.floor(s.length / 2)]
+    : s[Math.floor(s.length / 2 - 1)] + s[Math.floor(s.length / 2)];
+}
