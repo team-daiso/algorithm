9a046f023c78046fa9ef433800f6f8fb3672ffcc
dohyeons <ddhhss0603@gmail.com>
2023-02-02 15:58:27 +0900

    [Lv 1] 음양 더하기 - 파일 교체

--- hyeonsu/week 5/[Lv 1] 음양 더하기.js
+++ hyeonsu/week 5/[Lv 1] 음양 더하기.js
@@ -1,6 +1,7 @@
-function solution(arr) {
-	// 젤 작은 수의 index를 찾아서 splice로 잘라버리자~
-	if (arr.length === 1) return [-1]
-	arr.splice(arr.indexOf(Math.min(...arr)),1);
-	return arr;
+function solution(absolutes, signs) {
+	let nums = absolutes.map((el, idx) => {
+			return signs[idx] ? el : - el
+	});
+	
+	return nums.reduce((acc, cur) => acc + cur, 0);
 }
