a56aed5a5ea2bfe7ce59344f9a17784f561f2813
uxolrv <uxolrv@gmail.com>
2023-02-08 17:19:57 +0900

    [Lv 1] 문자열 다루기 기본 (리팩터링)

--- week6/[Lv 1] 문자열 다루기 기본.js
+++ week6/[Lv 1] 문자열 다루기 기본.js
@@ -1,6 +1,6 @@
 function solution(s) {
-  // \D는 [^0-9]과 동일
-  const regex = /\D/g;
+  const regex = /\D/g; // /[^0-9]/g 와 동일
+
   // 길이가 4, 6이 아니거나, 숫자가 아닌 문자가 발견된다면 false
-  return ![4, 6].includes(s.length) ? false : !regex.test(s);
+  return ![4, 6].includes(s.length) && !regex.test(s);
 }
