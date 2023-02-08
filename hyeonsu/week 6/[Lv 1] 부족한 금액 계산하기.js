26392d5ef678a965aefe6e4da17948342af95c1d
dohyeons <ddhhss0603@gmail.com>
2023-02-08 13:45:31 +0900

    [Lv 1] 부족한 금액 계산하기

---
+++ hyeonsu/week 6/[Lv 1] 부족한 금액 계산하기.js
@@ -0,0 +1,16 @@
+function solution(price, money, count) {
+	var answer = -1;
+	// 놀이기구를 n번 이용하면 price * n 이 이용요금이 됨
+	// 그렇다면 count번 탔을 때 가지고있는 금액은 얼마나 모자랄지? 
+	// 만약 안모자라면 0 리턴
+	let firstPrice = price
+	for(let i = 2; i<=count ; i++ ) {
+			price= price + firstPrice * i
+	}
+	if(price < money) {
+			answer = answer +1
+	} else {
+			answer = answer + price - money +1
+	}
+	return answer;
+}
