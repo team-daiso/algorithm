9d568f531ea3db206c5c7cec39b280f2a952c752
uxolrv <uxolrv@gmail.com>
2023-02-08 17:15:43 +0900

    [Lv 2] 영어 끝말잇기 (리팩터링)

--- week6/[Lv 2] 영어 끝말잇기.js
+++ week6/[Lv 2] 영어 끝말잇기.js
@@ -1,18 +1,18 @@
 function solution(n, words) {
-  const previousWords = [words[0]];
+  const previous = [words[0]];
   let turn = 1;
 
   for (let i = 1; i < words.length; i++) {
     const currentFirst = words[i][0];
-    const previousLast = previousWords[i - 1][previousWords[i - 1].length - 1];
+    const previousLast = previous[i - 1][previous[i - 1].length - 1];
 
     if (i % n === 0) turn++;
 
-    if (currentFirst !== previousLast || words[i].length === 1 || previousWords.includes(words[i])) {
+    if (currentFirst !== previousLast || previous.includes(words[i])) {
       return [(i % n) + 1, turn]; // 0번째 인덱스부터 첫번째 사람이기 때문에 +1
     }
 
-    previousWords[i] = words[i];
+    previous[i] = words[i];
   }
   return [0, 0];
 }
