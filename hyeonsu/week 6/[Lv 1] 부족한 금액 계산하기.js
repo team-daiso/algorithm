e40c33e66d5e0fd579f77d80dd149ab3dda9dc5e
dohyeons <ddhhss0603@gmail.com>
2023-02-08 14:44:19 +0900

    [Lv 1] 부족한 금액 계산 - 삼항연산자 적용

--- hyeonsu/week 6/[Lv 1] 부족한 금액 계산하기.js
+++ hyeonsu/week 6/[Lv 1] 부족한 금액 계산하기.js
@@ -1,16 +1,12 @@
 function solution(price, money, count) {
-	var answer = -1;
-	// 놀이기구를 n번 이용하면 price * n 이 이용요금이 됨
-	// 그렇다면 count번 탔을 때 가지고있는 금액은 얼마나 모자랄지? 
-	// 만약 안모자라면 0 리턴
-	let firstPrice = price
-	for(let i = 2; i<=count ; i++ ) {
-			price= price + firstPrice * i
-	}
-	if(price < money) {
-			answer = answer +1
-	} else {
-			answer = answer + price - money +1
-	}
-	return answer;
+  var answer = -1;
+  // 놀이기구를 n번 이용하면 price * n 이 이용요금이 됨
+  // 그렇다면 count번 탔을 때 가지고있는 금액은 얼마나 모자랄지?
+  // 만약 안모자라면 0 리턴
+  let firstPrice = price;
+  for (let i = 2; i <= count; i++) {
+    price += firstPrice * i;
+  }
+  price < money ? (answer += 1) : (answer += price - money + 1);
+  return answer;
 }
