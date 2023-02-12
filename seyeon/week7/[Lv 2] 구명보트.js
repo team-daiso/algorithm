c6ac3e2dbe3c37e5b7e253cd02291d96e7cecf0c
uxolrv <uxolrv@gmail.com>
2023-02-12 17:04:40 +0900

    [Lv 2] 구명보트

---
+++ seyeon/week7/[Lv 2] 구명보트.js
@@ -0,0 +1,15 @@
+function solution(people, limit) {
+  let count = 0;
+  people.sort((a, b) => b - a);
+
+  for (let weight of people) {
+    if (weight + people[people.length - 1] <= limit) {
+      people.pop();
+      count++;
+    } else {
+      count++;
+    }
+  }
+
+  return count;
+}
