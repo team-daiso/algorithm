91ea85a4b4b9e0f47dc107c903373cadf09ec8b9
seoyeon <seoyeon@gimseoyeon-ui-MacBookAir.local>
2023-02-15 00:49:51 +0900

    [Lv 1] 같은 숫자는 싫어

---
+++ seoyeon/week7/[Lv 1] 같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr){
+    let answer = [arr[0]];
+
+    for (let i = 1; i < arr.length; i++){
+        if (answer[answer.length -1] !== arr[i]){
+            answer.push(arr[i])
+        }
+    }
+    return answer;
+}
