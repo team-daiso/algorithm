9b1c136af7dd6a6e2a02cac24c22cdaae19ed367
uxolrv <uxolrv@gmail.com>
2023-02-15 21:57:33 +0900

    [Lv 2] N개의 최소공배수 (리팩터링)

--- seyeon/week7/[Lv 2] N개의 최소공배수.js
+++ seyeon/week7/[Lv 2] N개의 최소공배수.js
@@ -1,15 +1,19 @@
-// 최소공약수를 구하는 함수
-function gcd(a, b) {
-  return a % b === 0 ? b : gcd(b, a % b);
-}
-
 function solution(arr) {
   let lcm = arr[0];
 
   // a와 b의 최소공배수 = a * b / a와 b의 최대공약수
-  for (let i = 0; i < arr.length - 1; i++) {
-    lcm *= arr[i + 1] / gcd(lcm, arr[i + 1]);
+  for (let i = 1; i < arr.length; i++) {
+    lcm *= arr[i] / gcd(lcm, arr[i]);
   }
 
   return lcm;
+}
+
+function gcd(a, b) {
+  return a % b === 0 ? b : gcd(b, a % b);
+}
+
+// reduce 사용도 가능!
+function solution(arr) {
+  return arr.reduce((acc, cur) => (acc * cur) / gcd(acc, cur));
 }
