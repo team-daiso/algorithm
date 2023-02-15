69c25b91d85da66afb52efb87bbae7718f0dea97
uxolrv <uxolrv@gmail.com>
2023-02-15 21:48:21 +0900

    [Lv 1] 최대공약수와 최소공배수 (함수 전역으로 이동)

--- seyeon/week7/[Lv 1] 최대공약수와 최소공배수.js
+++ seyeon/week7/[Lv 1] 최대공약수와 최소공배수.js
@@ -1,16 +1,16 @@
 function solution(n, m) {
-  const getGCD = (n, m) => {
-    return n % m === 0 ? m : getGCD(m, n % m);
-  };
-
   const gcd = getGCD(n, m);
-
-  const getLCM = (n, m) => {
-    return (n * m) / gcd;
-  };
 
   return [gcd, getLCM(n, m)];
 }
+
+const getGCD = (n, m) => {
+  return n % m === 0 ? m : getGCD(m, n % m);
+};
+
+const getLCM = (n, m) => {
+  return (n * m) / gcd;
+};
 
 /*
 유클리드 호제법
