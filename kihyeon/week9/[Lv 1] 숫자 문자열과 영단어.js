231d7a0394d8490f7fc7cdae6e360c0f1982e2a1
kihyeoon <bangion93@gmail.com>
2023-02-26 15:16:05 +0900

    [Lv 1] 숫자 문자열과 영단어

---
+++ kihyeon/week9/[Lv 1] 숫자 문자열과 영단어.js
@@ -0,0 +1,18 @@
+function solution(str) {
+  const stringToNum = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  for (let i = 0; i < 10; i++) {
+    str = str.split(stringToNum[i]).join(i);
+  }
+  return parseInt(str);
+}
