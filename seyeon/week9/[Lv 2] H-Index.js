85a6b8abf53cfd33cfc2a41741618df9689ab21b
uxolrv <uxolrv@gmail.com>
2023-03-02 10:54:27 +0900

    [Lv 2] H-Index (리팩터링)

--- seyeon/week9/[Lv 2] H-Index.js
+++ seyeon/week9/[Lv 2] H-Index.js
@@ -12,3 +12,13 @@ function solution(citations) {
 
   return result;
 }
+
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] <= i) return i;
+  }
+
+  return citations.length;
+}
