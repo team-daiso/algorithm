fc95cdd8a0767c01fb148c523a241c6bde558e37
uxolrv <uxolrv@gmail.com>
2023-03-02 10:26:05 +0900

    [Lv 1] 숫자 문자열과 영단어 (리팩터링)

--- seyeon/week9/[Lv 1] 숫자 문자열과 영단어.js
+++ seyeon/week9/[Lv 1] 숫자 문자열과 영단어.js
@@ -2,7 +2,7 @@ function solution(s) {
   const engNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
   for (let i = 0; i < engNum.length; i++) {
-    if (s.includes(engNum[i])) s = s.replaceAll(engNum[i], i);
+    s = s.replaceAll(engNum[i], i);
   }
 
   return +s;
