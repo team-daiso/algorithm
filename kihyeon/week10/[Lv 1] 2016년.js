6d28789999ba410936c4fc25ba0f384adc8da87c
kihyeoon <bangion93@gmail.com>
2023-03-04 02:16:27 +0900

    [Lv 1] 2016년 - 추가 풀이

--- kihyeon/week10/[Lv 1] 2016년.js
+++ kihyeon/week10/[Lv 1] 2016년.js
@@ -6,3 +6,9 @@ function solution(a, b) {
 
   return week[accDays % 7];
 }
+
+function getDayName(a, b) {
+  var tempDate = new Date(2016, a - 1, b);
+
+  return tempDate.toString().slice(0, 3).toUpperCase();
+}
