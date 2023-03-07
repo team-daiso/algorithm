8068bd3ee436aa9c3523b8154eee2f96f67eef7c
KimYunhee <62630941+kyh1685@users.noreply.github.com>
2023-03-07 23:31:48 +0900

    Revert "김윤희 / 9주차 / 7문제"

--- yunhee/week9/ [Lv 1] 문자열 내 마음대로 정렬하기.js
+++
@@ -1,9 +0,0 @@
-function solution(strings, n) {
-    return strings.map(str => str[n] + str)
-                  .sort()
-                  .map(str => str.substring(1));
-}
-
-/*
-	n번째 인덱스가 같은 경우 사전순으로 정렬해줘야하므로 n번째 인덱스를 맨앞에 붙여주고 정렬한 다음 맨 앞에 붙였던 문자를 제거해준다!
-*/
