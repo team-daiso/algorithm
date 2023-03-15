5cb4e10f92cad6aa7d5123e1d39c391e7df61c83
uxolrv <uxolrv@gmail.com>
2023-03-15 17:21:57 +0900

    [Lv 1] 가장 가까운 같은 글자 (리팩터링)

--- seyeon/week11/[Lv 1] 가장 가까운 같은 글자.js
+++ seyeon/week11/[Lv 1] 가장 가까운 같은 글자.js
@@ -1,10 +1,6 @@
 function solution(s) {
-  let result = [];
-
-  [...s].map((char, i) => {
+  return [...s].map((char, i) => {
     const idx = s.slice(0, i).lastIndexOf(char);
-    result[i] = idx === -1 ? -1 : i - idx;
+    return idx === -1 ? idx : i - idx;
   });
-
-  return result;
 }
