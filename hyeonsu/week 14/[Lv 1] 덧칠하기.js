b6f9c4c2e928da99f3bf73bfb6ab02b5d51cb180
dohyeons <ddhhss0603@gmail.com>
2023-04-05 03:25:51 +0900

    [Lv 1] 덧칠하기

---
+++ hyeonsu/week 14/[Lv 1] 덧칠하기.js
@@ -0,0 +1,13 @@
+function solution(n, m, section) {
+  let answer = 0;
+  // 몇번 벽까지 페인트 칠 되어있는지?
+  let painted = 0;
+  for (let el of section) {
+    // 스스로도 포함하기 때문에 1을 빼줘야 함...2번에서부터 4개 색칠하면 5까지임.
+    if (painted < el) {
+      painted = m + el - 1;
+      answer++;
+    }
+  }
+  return answer;
+}
