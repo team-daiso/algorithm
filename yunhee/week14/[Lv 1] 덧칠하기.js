9d9e6c8179590b40a1f562c9677d7484a14e7060
KimYunhee <qnvnfls@naver.com>
2023-04-06 21:05:31 +0900

    [Lv 1] 덧칠하기 - for of문으로 변경

--- yunhee/week14/[Lv 1] 덧칠하기.js
+++ yunhee/week14/[Lv 1] 덧칠하기.js
@@ -6,12 +6,12 @@ function solution(n, m, sections) {
     let current = sections[0] + m; // 첫번째 구역에서 페인트칠 한번
     
     // 칠해야 하는 구역이 흩어져있으면
-    for(let i = 1; i < sections.length; i++){
+    for(let section of sections){
         // 현재 구역이 이전에 페인트칠 한 범위 안에 있다면 넘김
-        if(sections[i] < current) continue;
+        if(section < current) continue;
         // 이전 페인트칠 범위를 넘었다면 이번 구역에 페인트칠을 새로 해줌
         else {
-            current = sections[i] + m;
+            current = section + m;
             paintCnt++;
         }
     }
