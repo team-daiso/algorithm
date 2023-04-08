b797a6904b06f739839143522b60059a2af7388b
seoyeon <seoyeon@gimseoyeon-ui-MacBookAir.local>
2023-04-08 10:57:17 +0900

    [Lv 1] 추억점수

--- seoyeon/week15/[Lv 1] 추억점수.js
+++ seoyeon/week15/[Lv 1] 추억점수.js
@@ -1,14 +1,9 @@
-function solution(name, yearning, photos) {
-    const scores = [];
-  
-    for (let photo of photos) {
-      const sum = photo.reduce((acc, cur) => {
-        const idx = name.indexOf(cur);
-        return acc + (yearning[idx] || 0);
-      }, 0);
-  
-      scores.push(sum);
-    }
-  
-    return scores;
-  }
+function solution(name, yearning, photo) {
+    let score = {};
+    
+    name.forEach((el,idx) => score[el] = yearning[idx]);
+    
+    return photo.map(el => 
+              el.reduce((acc,cur) => acc + (score[cur] ? score[cur] : 0) ,0)
+					 )
+}
