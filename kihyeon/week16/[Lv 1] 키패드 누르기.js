42b6977b1e856546a39068ff0550ea28b2661a76
kihyeoon <bangion93@gmail.com>
2023-04-19 05:01:30 +0900

    [Lv 1] 키패드 누르기

---
+++ kihyeon/week16/[Lv 1] 키패드 누르기.js
@@ -0,0 +1,51 @@
+function solution(numbers, hand) {
+  let leftNum = 10;
+  let rightNum = 12;
+  return numbers
+    .map((num) => {
+      if (num === 0) {
+        num = 11;
+      }
+      if (num % 3 === 1) {
+        return leftTo(num);
+      } else if (num % 3 === 0) {
+        return rightTo(num);
+      } else {
+        const numLocation = numToLocation(num);
+        const leftDistance = distanceBtwLocation(
+          numToLocation(leftNum),
+          numLocation
+        );
+        const rightDistance = distanceBtwLocation(
+          numToLocation(rightNum),
+          numLocation
+        );
+        if (leftDistance === rightDistance) {
+          return hand === "left" ? leftTo(num) : rightTo(num);
+        } else if (leftDistance < rightDistance) {
+          return leftTo(num);
+        } else {
+          return rightTo(num);
+        }
+      }
+    })
+    .join("");
+
+  function leftTo(num) {
+    leftNum = num;
+    return "L";
+  }
+
+  function rightTo(num) {
+    rightNum = num;
+    return "R";
+  }
+}
+
+function numToLocation(num) {
+  return [Math.floor((num - 1) / 3), (num - 1) % 3];
+}
+
+function distanceBtwLocation(a, b) {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+}
