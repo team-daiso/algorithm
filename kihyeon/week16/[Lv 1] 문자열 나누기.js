2a90b6acb33307a2bbeae7bb6e18b5c5050b0f48
kihyeoon <bangion93@gmail.com>
2023-04-19 13:18:13 +0900

    [Lv 1] 문자열 나누기 - 리팩터링

--- kihyeon/week16/[Lv 1] 문자열 나누기.js
+++ kihyeon/week16/[Lv 1] 문자열 나누기.js
@@ -4,14 +4,14 @@ function solution(s) {
   let countNotX = 0;
   let answer = 0;
 
-  for (let i = 0; i < s.length; i++) {
+  [...s].forEach((_, i) => {
     if (x === "") x = s[i];
     s[i] === x ? countX++ : countNotX++;
     if (countX === countNotX) {
       answer++;
       x = "";
     }
-  }
+  });
   if (x !== "") answer++;
   return answer;
 }
